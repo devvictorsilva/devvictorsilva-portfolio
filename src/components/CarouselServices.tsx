e1d145d542958e271850763c25380460706e0e29
Victor <paulo9.victor.silva@gmail.com>
2025-06-24 16:18:43 -0300

    feat: add CarouselServices component and integrate it into the index page

---
+++ src/components/CarouselServices.tsx
@@ -0,0 +1,39 @@
+// services.tsx
+import { FaCode, FaMobileAlt, FaPaintBrush } from "react-icons/fa";
+import Carousel from "./Carousel/Carousel";
+
+export const services = [
+  {
+    title: "Web Development",
+    text: "Sou especialista na construção de aplicações web completas, do backend ao frontend. Utilizo tecnologias modernas como TypeScript, React, NestJS, PostgreSQL e Docker para entregar sistemas robustos, escaláveis e com ótimo desempenho. Valorizo código limpo, boas práticas e foco na experiência do usuário, buscando sempre entregar soluções práticas, visuais e bem arquitetadas.",
+    icon: <FaCode size={28} className="text-accent" />,
+  },
+  {
+    title: "Desenvolvimento de Interfaces Interativas (Frontend)",
+    text: "Tenho profundo conhecimento em design de interfaces funcionais e responsivas usando React, Angular e Tailwind CSS. Meu foco é transformar ideias em experiências visuais intuitivas e fluidas. Trabalho com atenção a microinterações, performance e acessibilidade, garantindo que cada componente visual tenha propósito e emoção.",
+    icon: <FaMobileAlt size={28} className="text-accent" />,
+  },
+  {
+    title: "Criação de Backends Modernos e APIs",
+    text: "Com NestJS e Django, desenvolvo APIs REST e serviços backend bem estruturados, seguros e fáceis de escalar. Integro bancos de dados relacionais como PostgreSQL e uso Docker para garantir ambientes estáveis. Busco criar sistemas organizados, com testes e autenticação, prontos para qualquer desafio.",
+    icon: <FaMobileAlt size={28} className="text-accent" />,
+  },
+  {
+    title: "Integração e Gerenciamento de Projetos Reais",
+    text: "Tenho experiência com projetos reais e em produção, incluindo sistemas para empresas como BEQ Energia, Acqualog e Lameff. Atuo desde a análise até o deploy, acompanhando o ciclo de vida completo da aplicação. Isso me permite entregar soluções reais que atendem demandas complexas com segurança e confiabilidade.",
+    icon: <FaPaintBrush size={28} className="text-accent" />,
+  },
+  {
+    title: "Testes e Qualidade de Código",
+    text: "Tenho afinidade com testes automatizados (como Jest) e práticas de versionamento limpo. Busco garantir que meu código não apenas funcione, mas seja confiável a longo prazo. Sempre que possível, aplico boas práticas de CI/CD e escrevo código que outros desenvolvedores conseguem manter com facilidade.",
+    icon: <FaPaintBrush size={28} className="text-accent" />,
+  },
+];
+
+export default function CarouselServices() {
+  return (
+    <Carousel
+      items={services}
+    />
+  )
+}
