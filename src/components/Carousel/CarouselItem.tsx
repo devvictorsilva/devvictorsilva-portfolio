07f263e842fbab0cea53f1b5830eb3eb05ea17e2
Victor <paulo9.victor.silva@gmail.com>
2025-06-28 22:47:15 -0300

    feat: enhance CarouselItem layout for improved responsiveness and styling

--- src/components/Carousel/CarouselItem.tsx
+++ src/components/Carousel/CarouselItem.tsx
@@ -10,14 +10,14 @@ export default function CarouselItem({ icon, title, text, isSelected }: Carousel
     <div
       className={`
         reveal w-full h-full text-white flex flex-col gap-4
-        pt-6 pb-10 px-8 cursor-pointer 
+        pt-6 pb-5 desktop:pb-10 px-2 desktop:px-8 cursor-pointer 
         border transition-all duration-400 ease-in-out
         ${isSelected ? "radiant shadow-glow" : "grounded-radiants"}
       `}
     >
-      <div className="flex items-center gap-4 mb-2">
+      <div className="flex flex-col desktop:flex-row items-center gap-4 mb-2">
         {icon && <div className="text-accent">{icon}</div>}
-        <h2 className="text-white font-semibold text-2xl leading-relaxed">{title}</h2>
+        <h2 className="text-white font-semibold text-center desktop:text-justify text-2xl leading-relaxed">{title}</h2>
       </div>
       <p className="text-white text-justify text-base px-4">{text}</p>
     </div>
